4ea092f9d6e33ab8b8619d281345626d95d603a1
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2020-12-14 23:12:49 +0200

    Deleted the drawing board function
    We'll use a different approach on that.

--- L Game/Multiplayer.cpp
+++ L Game/Multiplayer.cpp
@@ -11,30 +11,3 @@ void startMultiplayerGame()
     while(true)
         continue;
 }
-
-void drawBoard()
-{
-    ///Get the length and the width of the screen.
-    int length = GetSystemMetrics(SM_CXSCREEN);
-    int width = GetSystemMetrics(SM_CYSCREEN);
-    int left = width / 2 - 150,right = width + width / 2 - 150, bottom = width - 100,top = 50;
-    int midX = (left + right) / 2;
-    int midY = (top + bottom) / 2;
-
-    rectangle(left,top,right,bottom);
-    line(midX, top, midX, bottom);
-    line(right, midY, left, midY);
-
-    midX = (left + midX) / 2;
-    midY = (top + midY) / 2;
-
-    line(midX, top, midX, bottom);
-    line(right, midY, left, midY);
-
-
-    ///TODO: Change values here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    midX = (left + right) / 2 + midX;
-    midY = (top + bottom) / 2 + midY;
-    line(midX, top, midX, bottom);
-    line(right, midY, left, midY);
-}
--- L Game/Multiplayer.h
+++ L Game/Multiplayer.h
@@ -7,6 +7,17 @@ class LObj///the L object on the board
         unsigned short shape;///when we'll extend the game we'll add more shapes
 };
 
+class Cell
+{
+    private:
+        int dim = 200;///200 x 200 px each cell
+    public:
+        void changeColor(int player)
+        {
+
+        }
+
+};
 
 void drawBoard();
 void startMultiplayerGame();
