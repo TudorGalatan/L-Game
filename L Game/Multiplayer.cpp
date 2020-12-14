9571aea33b0563b3c99db66a64849db87e73f6a5
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2020-12-14 22:49:16 +0200

    Draws a part of the board
    I want to go to sleep so i pushed this code
    It doesn t display good the whole board, but i ll fix it in the morning I guess

--- L Game/Multiplayer.cpp
+++ L Game/Multiplayer.cpp
@@ -20,7 +20,21 @@ void drawBoard()
     int left = width / 2 - 150,right = width + width / 2 - 150, bottom = width - 100,top = 50;
     int midX = (left + right) / 2;
     int midY = (top + bottom) / 2;
+
     rectangle(left,top,right,bottom);
+    line(midX, top, midX, bottom);
+    line(right, midY, left, midY);
+
+    midX = (left + midX) / 2;
+    midY = (top + midY) / 2;
+
+    line(midX, top, midX, bottom);
+    line(right, midY, left, midY);
+
+
+    ///TODO: Change values here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    midX = (left + right) / 2 + midX;
+    midY = (top + bottom) / 2 + midY;
     line(midX, top, midX, bottom);
     line(right, midY, left, midY);
 }
