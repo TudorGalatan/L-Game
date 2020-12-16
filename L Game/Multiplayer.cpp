eff33c831d8d844ce2cbead9e157f93ee5692782
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2020-12-16 21:16:46 +0200

    Colors the board with the L's + added a coin class
    I added the colors for the L's and I created a class for the coins

--- L Game/Multiplayer.cpp
+++ L Game/Multiplayer.cpp
@@ -27,10 +27,14 @@ int Cell::changeCellSize(int newSize)
     dim = newSize;
     return dim;
 }
-
+void Cell::setPosition(int pX,int pY)
+{
+    posX = pX;
+    posY = pY;
+}
 void drawBoard()
 {
-    Cell cell[5][5];
+    Cell cell[4][4];
     int xCoord = 0, yCoord = 0;
     int height = 1080;
     int cellSize = (height - 200) / 4;///calculate the size of a cell
@@ -41,10 +45,18 @@ void drawBoard()
         {
             cell[xCoord][yCoord].drawCell(i, j,i + cellSize, j + cellSize);///draw the cell
             cell[xCoord][yCoord].setPosition(i + cellSize / 2, j + cellSize / 2);
-            cell[xCoord][yCoord].setColor(BLACK);///E CV MIC GRESIT AICI LA COLORAT
-            yCoord++;
+            cell[xCoord][yCoord].setColor(BLACK);///set the black color to the cells
+            xCoord++;
         }
-        yCoord = 0;
-        xCoord++;
+        xCoord = 0;
+        yCoord++;
     }
+    ///Draw the red L.
+    cell[1][0].setColor(RED);
+    for(int i=0;i<3;i++)
+        cell[2][i].setColor(RED);
+    ///Draw the blue L.
+    cell[2][3].setColor(BLUE);
+    for(int i=1;i<=3;i++)
+        cell[1][i].setColor(BLUE);
 }
--- L Game/Multiplayer.h
+++ L Game/Multiplayer.h
@@ -5,26 +5,28 @@
 class LObj///the L object on the board
 {
     private:
-        unsigned short color;
+        unsigned short color;///4 is RED, 1 is BLUE
         unsigned short shape;///when we'll extend the game we'll add more shapes
+};
+
+class Coin
+{
+    private:
+        int coinSize;
+    public:
+        void drawCoin();
 };
 
 class Cell///a cell on the board
 {
     public:
          int dim;///200 x 200 px each cell
-         int posX, posY;
+         int posX, posY;///we store the positions used in setPosition()
     public:
-        void setPosition(int pX,int pY)
-        {
-            posX = pX;
-            posY = pY;
-        }
-
+        void setPosition(int pX,int pY);///set the position to help the setColor function fill the right cell
         void setColor(int color);///set the color of the cell
-        void drawCell(int left,int up,int right,int bottom);
-        int changeCellSize(int newSize);
-
+        void drawCell(int left,int up,int right,int bottom);///draw the rectangle
+        int changeCellSize(int newSize);///change the size of the cell (not used yet)
 };
 
 void drawBoard();
