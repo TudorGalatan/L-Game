589832c977744d0c60378b89a9171de886c8f660
Tudor Gălățan <tudorgalatan@gmail.com>
2020-12-17 00:41:34 +0200

    Updated the Application_Manager.cpp File
    I've updated the Application_Manager.cpp file.

--- L Game/Application_Manager.cpp
+++ L Game/Application_Manager.cpp
@@ -5,20 +5,16 @@
 
 #include "Test.h"
 #include "User_Interface.h"
-// #include "Controller.h"         // Temporary (Working Area)
 
 
 
 int main ()
 {
-    // Test all the functionalities of the application.
+    // Test the application.
     runTests();
 
     // Start the application.
     runGUI();
-
-    // Temporary (Working Area)
-    // runApplication();
 
     return 0;
 }
