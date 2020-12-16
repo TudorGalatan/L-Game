9a30786fcbc730ec7c98eee08a2c5acfdee68e60
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2020-12-16 23:27:06 +0200

    Added action on click
    I added action on mouse click, started the redMove function, created a function that draws a coin(it s hard coded at this time)

--- L Game/Multiplayer.cpp
+++ L Game/Multiplayer.cpp
@@ -2,18 +2,36 @@
 
 #include <graphics.h>
 #include <iostream>
-#include <windows.h>
+#include <Windows.h>
 
 void startMultiplayerGame()
 {
+    Cell cell[4][4];
+    bool player = false;///False -> RED PLAYER, True -> BLUE PLAYER
     cleardevice();
-    drawBoard();
+    drawBoard(cell);
+    delay(1000);///Just for testingm
     while(true)
-        continue;
+    {
+        switch(player)
+        {
+            case false:
+                ///RED TURN
+                redMove(cell);
+                player = !player;
+                break;
+            default:
+                ///BLUE TURN
+                player = !player;
+        }
+    }
 }
-
+/**
+    Cell class functions.
+*/
 void Cell::setColor(int color)///set the color of the cell
 {
+    cellColor = color;
     setfillstyle(SOLID_FILL, color);///set the fill style
     floodfill(posX,posY,WHITE);///color the cell
 }
@@ -32,11 +50,30 @@ void Cell::setPosition(int pX,int pY)
     posX = pX;
     posY = pY;
 }
-void drawBoard()
+int Cell::getColor()
 {
-    Cell cell[4][4];
+    return cellColor;
+}
+///---------------------------------------------------------------
+/**
+    Coin class functions.
+*/
+void Coin::drawCoin()///we have to change this function to be more flexible-----> pls ask me wtf i meant by that
+{
+    int height = GetSystemMetrics(SM_CYSCREEN);
+    int cellSize = (height - 200) / 4;
+    int posX = height / 4 + cellSize + cellSize / 2;
+    int posY = height / 4 - cellSize + cellSize / 2;
+    circle(posX, posY, getCoinSize());
+    setfillstyle(SOLID_FILL, YELLOW);///set the fill style
+    floodfill(posX,posY,WHITE);///color the cell
+}
+///---------------------------------------------------------------
+void drawBoard(Cell cell[][4])
+{
+    Coin coin1,coin2;
     int xCoord = 0, yCoord = 0;
-    int height = 1080;
+    int height = GetSystemMetrics(SM_CYSCREEN);
     int cellSize = (height - 200) / 4;///calculate the size of a cell
     cellSize = cell[xCoord][yCoord].changeCellSize(cellSize);///change the value in the class
     for(int i = height / 4 + cellSize; i <= height / 4 + 4 * cellSize; i += cellSize)
@@ -51,6 +88,8 @@ void drawBoard()
         xCoord = 0;
         yCoord++;
     }
+    ///Draw the coin from the top-left
+    coin1.drawCoin();
     ///Draw the red L.
     cell[1][0].setColor(RED);
     for(int i=0;i<3;i++)
@@ -59,4 +98,15 @@ void drawBoard()
     cell[2][3].setColor(BLUE);
     for(int i=1;i<=3;i++)
         cell[1][i].setColor(BLUE);
+}
+void redMove(Cell cell[][4])
+{
+    if(GetAsyncKeyState(VK_LBUTTON))
+    {
+        int playerColor = RED;
+        for(int i=0;i<4;i++)
+            for(int j=0;j<4;j++)
+                if(cell[i][j].getColor() == RED)
+                    cell[i][j].setColor(BLACK);
+    }
 }
