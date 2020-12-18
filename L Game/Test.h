faab4e76644399e067d3ba9a58518d36e3b6bbcb
Tudor Gălățan <tudorgalatan@gmail.com>
2020-12-18 22:28:22 +0200

    Improved the "Test" Module
    I've improved the "Test" module.

--- L Game/Test.h
+++ L Game/Test.h
@@ -1,10 +1,17 @@
 /**
-    This is the interface for the Test layer.
-    The Test layer is responsible with testing the functionalities of the application.
+    This is the interface for the Test module.
+    The module is responsible with testing the functionalities of the application.
 **/
 
 #pragma once
 
 
 
+/**
+    Runs all the tests.
+    Input:
+        - none
+    Output:
+        - runs all the tests
+**/
 void runTests ();
