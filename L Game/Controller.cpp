c50c9df477d987a9230969a207633f8a7038dd36
Tudor Gălățan <tudorgalatan@gmail.com>
2020-12-19 01:55:03 +0200

    Improved the "Controller" Module
    I've improved the "Controller" module.

--- L Game/Controller.cpp
+++ L Game/Controller.cpp
@@ -1,6 +1,6 @@
 /**
-    This is the implementation for the Controller layer.
-    The Controller layer is responsible with coordinating the application.
+    This is the implementation for the Controller module.
+    The module is responsible with coordinating the application.
 **/
 
 #include "Controller.h"
@@ -9,18 +9,8 @@
 #include <fstream>
 #include <iostream>
 
-/**
-    Reads the data from the input file.
-    Input:
-        - "gameBoard": the game board
-    Output:
-        - none
-    Observations:
-        - 0: empty square
-        - 1: the red player
-        - 2: the blue player
-        - 3: coin
-**/
+
+
 void readData (USI gameBoard[][4], GameBoard board)
 {
     std::ifstream inputFile("Input.in");
@@ -32,13 +22,6 @@ void readData (USI gameBoard[][4], GameBoard board)
 
 
 
-/**
-    Prints the game board on the screen.
-    Input:
-        - "gameBoard": the game board
-    Output:
-        - none
-**/
 void printBoard (USI gameBoard[][4])
 {
     std::ofstream saveFile("Playground.txt");
@@ -52,13 +35,6 @@ void printBoard (USI gameBoard[][4])
 
 
 
-/**
-    Get the coordinates of the new "L" from the user.
-    Input:
-        - none
-    Output:
-        - "coordinates": the line and column coordinates of the new "L"
-**/
 void getNewCoordinates (std::vector < std::pair <USI, USI> >& coordinates)
 {
     std::cout << "\nGive the coordinates of your new L:\n";
@@ -73,18 +49,12 @@ void getNewCoordinates (std::vector < std::pair <USI, USI> >& coordinates)
 
 
 
-/**
-    Checks whether the coordinates can represent a valid move on the game board. So it checks if it is an "L".
-    Input:
-        - "gameBoard": the game board
-        - "currentPlayer": the current player
-        - "coordinates": the coordinates
-    Output:
-        - true: if the coordinates represent a valid move on the game board
-        - false: otherwise
-**/
 bool checkMove (USI gameBoard[][4], USI currentPlayer, std::vector < std::pair <USI, USI> > coordinates)
 {
+    // If not all cells are empty, then it cannot be a valid move.
+    if (emptyCells(gameBoard, coordinates) == false)
+        return false;
+
     std::pair <USI, USI> orientation = getOrientation(gameBoard, coordinates);
 
     // If we have neither a horizontal, nor a vertical orientation, then it is not an "L".
@@ -102,25 +72,30 @@ bool checkMove (USI gameBoard[][4], USI currentPlayer, std::vector < std::pair <
     if (onValidPosition(gameBoard, orientation, startEndPositions, currentPlayer) == 0)
         return false;
 
-    // We have an "L" form, so it is a valid move.
+    // We have an "L" form with empty cells only, so it is a valid move.
     return true;
 }
 
 
 
-/**
-    Gets the orientation for the possible "L".
-    Input:
-        - "gameBoard": the game board
-        - "coordinates": the coordinates of the squares
-    Output:
-        - "orientation": a pair containing:
-            - "orientation.first": the orientation for the possible "L"
-                - 0: if the orientation for the possible "L" is neither horizontal, nor vertical
-                - 1: if the orientation for the possible "L" is horizontal
-                - 2: if the orientation for the possible "L" is vertical
-            - "orientation.second": the number of the line or the column that constitutes the body of "L"
-**/
+bool emptyCells (USI gameBoard[][4], std::vector < std::pair <USI, USI> > coordinates)
+{
+    for (unsigned short int cell = 0; cell < 4; cell++)
+    {
+        unsigned short int line = coordinates[cell].first;
+        unsigned short int column = coordinates[cell].second;
+
+        // We have a non-empty cell.
+        if (gameBoard[line][column] != 0)
+            return false;
+    }
+
+    // We have empty cells only.
+    return true;
+}
+
+
+
 std::pair <USI, USI> getOrientation (USI gameBoard[][4], std::vector < std::pair <USI, USI> > coordinates)
 {
     // The frequency of the lines and columns of the coordinates for the possible "L"
@@ -165,16 +140,6 @@ std::pair <USI, USI> getOrientation (USI gameBoard[][4], std::vector < std::pair
 
 
 
-/**
-    It checks if there is a gap between the three elements of the possible "L".
-    Input:
-        - "gameBoard": the game board
-        - "orientation": the orientation for the possible "L"
-        - "currentPlayer": the current player
-    Output:
-        - true: if there is a gap
-        - false: otherwise
-**/
 bool hasGap (USI gameBoard[][4], std::pair <USI, USI> orientation, USI currentPlayer)
 {
         // We have a horizontal orientation.
@@ -196,17 +161,6 @@ bool hasGap (USI gameBoard[][4], std::pair <USI, USI> orientation, USI currentPl
 
 
 
-/**
-    Gets the start and end positions for the body of the "L" form.
-    Input:
-        - "gameBoard": the game board
-        - "orientation": the orientation of "L"
-        - "currentPlayer": the current player
-    Output:
-        - "startEndPositions": a pair consisting of:
-            - "startEndPositions.first": the start position
-            - "startEndPositions.second": the end position
-**/
 std::pair <USI, USI> getStartEndPositions (USI gameBoard[][4], std::pair <USI, USI> orientation, USI currentPlayer)
 {
     // We assume the start position is 0.
@@ -239,17 +193,6 @@ std::pair <USI, USI> getStartEndPositions (USI gameBoard[][4], std::pair <USI, U
 
 
 
-/**
-    Checks whether the other square from the possible "L" is on a valid position.
-    Input:
-        - "gameBoard": the game board
-        - "orientation": the orientation of the possible "L"
-        - "startEndPositions": the start and end positions for the body of the possible "L"
-        - "currentPlayer": the current player
-    Output:
-        - true: if the other square is on a proper position
-        - false: otherwise
-**/
 bool onValidPosition (USI gameBoard[][4], std::pair <USI, USI> orientation, std::pair <USI, USI> startEndPositions, USI currentPlayer)
 {
     unsigned short int startPosition = startEndPositions.first;
@@ -277,7 +220,6 @@ bool onValidPosition (USI gameBoard[][4], std::pair <USI, USI> orientation, std:
         possibleCoordinates.push_back(std::make_pair(endPosition, column + 1));
     }
 
-
     for (unsigned short int possibleSquare = 0; possibleSquare < possibleCoordinates.size(); possibleSquare++)
     {
         unsigned short int line = possibleCoordinates.at(possibleSquare).first;
@@ -293,15 +235,6 @@ bool onValidPosition (USI gameBoard[][4], std::pair <USI, USI> orientation, std:
 
 
 
-/**
-    It performs the current move of the current player, considering that all validations were already done.
-    Input:
-        - "gameBoard": the game board
-        - "currentPlayer": the current player
-        - "newCoordinates": the new coordinates of the current player
-    Output:
-        - "gameBoard": the updated game board
-**/
 void makeMove (USI gameBoard[][4], USI currentPlayer, std::vector < std::pair <USI, USI> > newCoordinates)
 {
     // Delete the current squares of the current player.
