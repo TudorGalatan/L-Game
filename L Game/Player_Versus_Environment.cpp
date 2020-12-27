c40af00f7e2f75e61706d16e47dae64e569b2845
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2020-12-27 02:32:05 +0200

    The easy bot moves now the coin
    has a bug(doesn't delete the old coin yet)

--- L Game/Player_Versus_Environment.cpp
+++ L Game/Player_Versus_Environment.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <time.h>
 
 
 void PlayerVersusEnvironment::startGame ()
@@ -91,6 +92,26 @@ void PlayerVersusEnvironment::startGame ()
                         this->gameBoard.blueL.positions[i].first = pos[i].first;
                         this->gameBoard.blueL.positions[i].second = pos[i].second;
                     }
+                    bool changed = false;
+                    for(int i=0;i<4 && changed == false;i++)
+                        for(int j=0;j<4 && changed == false;j++)
+                            if(this->gameBoard.boardData[i][j]==0)
+                            {
+                                changed = true;
+                                srand(time(NULL));
+                                int val = rand()%2+1;
+                                Coin* obj;
+                                if(val==1)
+                                    obj=&this->gameBoard.firstCoin;
+                                else
+                                    obj=&this->gameBoard.secondCoin;
+                                obj->deleteCoin(this->gameBoard.cell[i][j].getPosition("ox"),this->gameBoard.cell[i][j].getPosition("oy"));
+                                this->gameBoard.boardData[i][j]=0;
+                                obj->setXIndex(this->gameBoard.cell[i][j].getPosition("ox"));
+                                obj->setYIndex(this->gameBoard.cell[i][j].getPosition("oy"));
+                                obj->drawCoin(obj->getXIndex(),obj->getYIndex());
+                                this->gameBoard.boardData[i][j]=3;
+                            }
                     //std::cout<<pos.size();
                 }
                 else
