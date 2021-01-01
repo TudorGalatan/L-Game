ffffdcbc21a8d25e00ac8cf8aa8a0ca6978e0de9
Tudor Gălățan <tudorgalatan@gmail.com>
2021-01-01 08:10:18 +0200

    Improved the Application Manager Module
    I've improved the Application Manager module.

--- L Game/Application_Manager.cpp
+++ L Game/Application_Manager.cpp
@@ -10,14 +10,13 @@
 
 int main ()
 {
-    // Create the necessary objects.
     Test test;
     UserInterface userInterface;
 
     // Test the application.
-    // test.runAll();
+    test.runAll();
 
-    // Start the application.
+    // Run the application.
     userInterface.startGUI();
 
     return 0;
